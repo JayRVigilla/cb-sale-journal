8398f53f2848e8f9b1c48767568dec1f5dd8b2b0
Jay Vigilla <JayRVigilla@gmail.com>
2020-10-03 10:39:44 -0700

    get_aqi() now funcitoning

--- aqi.py
+++ aqi.py
@@ -12,8 +12,12 @@ from secrets import IQ_AIR_KEY
 
 def get_aqi():
     """ makes request to IQ AIR for local AQI """
-    res = requests.get(f"""http://api.airvisual.com/v2/
-    city?city=Berkeley&state=California&country=USA&key={IQ_AIR_KEY}""",
+    res = requests.get(f"""http://api.airvisual.com/v2/nearest_city?key=71d21fba-0115-4854-94b1-60bdea46a3c5""",
                        timeout=1.0).text
-    json.load(res)
-    return res.data.current.pollution['aqius']
+    json_to_dict = json.loads(res)
+    print(f" \n\n\n ***** json_to_dict is {json_to_dict} ***** \n\n\n")
+    print(f" \n\n\n ***** type {type(json_to_dict)} ***** \n\n\n")
+    print(f" \n\n\n ***** keys {json_to_dict.keys()} ***** \n\n\n")
+    print(f" \n\n\n ***** data is {json_to_dict.get('data')} ***** \n\n\n")
+    # return json_to_dict.data.current.pollution['aqius']
+    return json_to_dict['data']['current']['pollution']['aqius']
